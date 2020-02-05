b8444f36357959aff352af4d98b86237e6a347fc
SilviuMihai <silviu96@gmail.com>
2020-02-05 22:39:44 +0200

    Added ContactList for BookStore, LoginPage, UserProfile.

---
+++ Models/ContactBookStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class ContactBookStore
+    {
+        public int PhoneNumber { get; set; }
+        public int Fax { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
