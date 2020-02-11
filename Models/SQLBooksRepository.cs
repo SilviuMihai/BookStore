32afc9a3872fedacbb973be8d69e1ef821baabe8
SilviuMihai <silviu96@gmail.com>
2020-02-11 07:15:33 +0200

    Changes that I can connect to a database !

---
+++ Models/SQLBooksRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class SQLBooksRepository : IBookStore
+    {
+        public IEnumerable<BooksDisplayed> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookStoreTime GetDateandTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,16 +8,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace BookStore
 {
     public class Startup
     {
+        public  Startup(IConfiguration configuration)
+	{
+        Configuration=configuration;
+	}
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<AppDBContext>(options => options.UseSqlServer());
+            services.AddDbContextPool<AppDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BooksDBConnection")));
             //services.AddMvc();
             services.AddControllersWithViews();
             services.AddRazorPages();
